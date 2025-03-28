5572e5d181e0709d922efe1a3c3f2e072521ec19
TonNom <ton-email@example.com>
2025-03-28 01:45:11 +0100

    add task of end of project

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+    constructor(sqft) {
+      this._sqft = sqft;
+  
+      if (this.constructor !== Building) {
+        if (!Object.prototype.hasOwnProperty.call(this.constructor.prototype, 'evacuationWarningMessage')) {
+          throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+      }
+    }
+  
+    get sqft() {
+      return this._sqft;
+    }
+  }

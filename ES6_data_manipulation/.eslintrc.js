e797ff46e36d8be50551c8b3f5b6624492816d17
TonNom <ton-email@example.com>
2025-08-25 21:31:36 +0200

    add list basic etudiant

--- ES6_data_manipulation/.eslintrc.js
+++ ES6_data_manipulation/.eslintrc.js
@@ -1,38 +1,16 @@
 module.exports = {
     env: {
-      browser: false,
-      es6: true,
+      es2021: true,
+      node: true,
       jest: true,
     },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
     parserOptions: {
-      ecmaVersion: 2018,
+      ecmaVersion: 'latest',
       sourceType: 'module',
     },
-    plugins: ['jest'],
+    extends: ['eslint:recommended'],
     rules: {
-      'max-classes-per-file': 'off',
-      'no-underscore-dangle': 'off',
       'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
     },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
   };
   

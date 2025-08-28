0de0792d22c1b68490e255f78c4653996a76873d
TonNom <ton-email@example.com>
2025-08-28 11:43:42 +0200

    task 1

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+// Retourne tjrs une Promise (vide) pour vérifier que c'est bien une instance de Promise
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      resolve();
+    });
+  }

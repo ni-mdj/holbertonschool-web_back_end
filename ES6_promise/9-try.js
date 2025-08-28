80da96fab290bc4b9706ef46d9ffe10944477eeb
TonNom <ton-email@example.com>
2025-08-28 14:24:59 +0200

    add task of end of project

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+// 9-try.js
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+      queue.push(mathFunction());
+    } catch (err) {
+      queue.push(`Error: ${err.message}`);
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+    return queue;
+  }

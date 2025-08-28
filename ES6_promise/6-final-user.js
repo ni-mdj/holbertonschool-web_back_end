40406ea84e80c9b387ecdea16ce0e81273c4a3cf
TonNom <ton-email@example.com>
2025-08-28 14:09:59 +0200

    add task of end of project

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+// Combine signUpUser et uploadPhoto : retourne un tableau avec le statut et la valeur/erreur de chaque Promise
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+}

92928dcd57520af3eff045883dec2c25760532f1
TonNom <ton-email@example.com>
2025-08-28 14:26:14 +0200

    add task of end of project

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,4 +1,3 @@
-// 7-load_balancer.js
 export default function loadBalancer(chinaDownload, USDownload) {
     return Promise.race([chinaDownload, USDownload]);
   }  
--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,4 +1,3 @@
-// 8-try.js
 export default function divideFunction(numerator, denominator) {
     if (denominator === 0) throw new Error('cannot divide by 0');
     return numerator / denominator;

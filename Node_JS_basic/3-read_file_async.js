1e9396c6aae7264636289b893ede9fb06973a6b4
TonNom <ton-email@example.com>
2025-09-03 23:01:11 +0200

    asynchrone and synchrone files

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,32 @@
+// asynchrone avec Promisees lecture d’un fichier CSV et affiche le nombre d’étudiants
+const fs = require("fs");
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(new Error("Cannot load the database"));
+        return;
+      }
+      const lines = data.trim().split("\n").slice(1);
+      const students = {};
+      lines.forEach((line) => {
+        const [firstname, , , field] = line.split(",");
+        if (field) {
+          if (!students[field]) students[field] = [];
+          students[field].push(firstname);
+        }
+      });
+
+      console.log(`Number of students: ${lines.length}`);
+      for (const field in students) {
+        console.log(
+          `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(", ")}`
+        );
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+// utilisation avec Express pour créer un serveur HTTP
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello Holberton School!");
+});
+
+app.listen(1245);
+
+module.exports = app;
